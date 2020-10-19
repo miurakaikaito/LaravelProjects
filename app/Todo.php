fa18bc910123a548de1cb3322458c7aabf71bd95
miurakaikaito <56814146+miurakaikaito@users.noreply.github.com>
2020-10-19 10:56:19 +0900

    変更

---
+++ app/Todo.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Todo extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * 日付へキャストする属性
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    protected $fillable = [
+        'title',
+        'user_id'
+    ];
+
+    public function getByUserId($id)
+    {
+        return $this->where('user_id', $id)->get();
+    }
+}
---
+++ resources/views/todo/index.blade.php
@@ -0,0 +1,36 @@
+@extends ('layouts.app')
+@section ('content')
+<h1 class="page-header">{{ Auth::user()->name }}ToDo一覧</h1>
+<p class="text-right">
+  <a class="btn btn-success" href="/todo/create">新規作成</a>
+</p>
+<table class="table">
+  <thead class="thead-light">
+    <tr>
+      <th>ID</th>
+      <th>やること</th>
+      <th>作成日時</th>
+      <th>更新日時</th>
+      <th></th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($todos as $todo)
+      <tr>
+        <td class="align-middle">{{ $todo->id }}</td>
+        <td class="align-middle">{{ $todo->title }}</td>
+        <td class="align-middle">{{ $todo->created_at }}</td>
+        <td class="align-middle">{{ $todo->updated_at }}</td>
+        <td><a class="btn btn-primary" href="{{ route('todo.edit', $todo->id) }}">編集</a></td>
+        <td>
+          {!! Form::open(['route' => ['todo.destroy', $todo->id], 'method' => 'DELETE']) !!}
+            {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
+          {!! Form::close() !!}
+        </td>
+      </tr>
+    @endforeach
+  </tbody>
+</table>
+
+@endsection
